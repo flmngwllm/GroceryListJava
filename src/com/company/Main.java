477a659c4d61c4fd0ff5462e507ccf42833b8972
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-02-24 18:38:36 -0500

    added a print method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -41,4 +41,17 @@ public class Main {
             }
         }
     }
+
+    public static void printInstructions(){
+        System.out.println("\nPress");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list");
+        System.out.println("\t 3 - To modify an item in the list");
+        System.out.println("\t 4 - To remove an item to the list");
+        System.out.println("\t 5 - To search an item to the list");
+        System.out.println("\t 6 - To quit the application");
+
+    }
+
 }
